ff8bc518c1d5bc4abac889f859bfc3e58e63d2fa
Jonel Dominic Tapang <joneldominictapang.dev@gmail.com>
2022-06-23 14:35:54 +0800

    Implement freezed and json_serializable for models

--- lib/core/models/person.dart
+++
@@ -1,9 +0,0 @@
-import 'package:personal_finance_management_app/core/enums/gender.dart';
-
-class Person {
-  final String name;
-  final int age;
-  final Gender gender;
-
-  Person(this.name, this.age, this.gender);
-}

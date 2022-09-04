2d5250ae0b9e425d15e746bae34681cf3e55e7fc
Jonel Dominic Tapang <joneldominictapang.dev@gmail.com>
2022-09-04 21:06:41 +0800

    Added CashFlowCard Widget

--- lib/ui/themes/custom_theme.dart
+++ lib/ui/themes/custom_theme.dart
@@ -10,6 +10,9 @@ class CustomTheme extends ThemeExtension<CustomTheme> {
     required this.scaffoldBackgroundColor,
     required this.appBarBackgroundColor,
     required this.primaryAccent,
+    required this.primaryTextColor,
+    required this.customGreen,
+    required this.customGrey,
   });
 
   final Color? success;
@@ -19,6 +22,9 @@ class CustomTheme extends ThemeExtension<CustomTheme> {
   final Color? scaffoldBackgroundColor;
   final Color? appBarBackgroundColor;
   final Color? primaryAccent;
+  final Color? primaryTextColor;
+  final Color? customGreen;
+  final Color? customGrey;
 
   @override
   CustomTheme copyWith({
@@ -29,6 +35,9 @@ class CustomTheme extends ThemeExtension<CustomTheme> {
     Color? scaffoldBackgroundColor,
     Color? appBarBackgroundColor,
     Color? primaryAccent,
+    Color? primaryTextColor,
+    Color? customGreen,
+    Color? customGrey,
   }) {
     return CustomTheme(
       success: success ?? this.success,
@@ -40,6 +49,9 @@ class CustomTheme extends ThemeExtension<CustomTheme> {
       appBarBackgroundColor:
           appBarBackgroundColor ?? this.appBarBackgroundColor,
       primaryAccent: primaryAccent ?? this.primaryAccent,
+      primaryTextColor: primaryTextColor ?? this.primaryTextColor,
+      customGreen: customGreen ?? this.customGreen,
+      customGrey: customGrey ?? this.customGrey,
     );
   }
 
@@ -59,6 +71,9 @@ class CustomTheme extends ThemeExtension<CustomTheme> {
       appBarBackgroundColor:
           Color.lerp(appBarBackgroundColor, other.appBarBackgroundColor, t),
       primaryAccent: Color.lerp(primaryAccent, other.primaryAccent, t),
+      primaryTextColor: Color.lerp(primaryTextColor, other.primaryTextColor, t),
+      customGreen: Color.lerp(customGreen, other.customGreen, t),
+      customGrey: Color.lerp(customGrey, other.customGrey, t),
     );
   }
 
@@ -71,6 +86,9 @@ class CustomTheme extends ThemeExtension<CustomTheme> {
       'scaffoldBackgroundColor: $scaffoldBackgroundColor, '
       'appBarBackgroundColor: $appBarBackgroundColor, '
       'primaryAccent: $primaryAccent, '
+      'primaryTextColor: $primaryTextColor, '
+      'customGreen: $customGreen, '
+      'customGrey: $customGrey, '
       ')';
 
   /// ****************************************************************
@@ -84,18 +102,24 @@ class CustomTheme extends ThemeExtension<CustomTheme> {
     scaffoldBackgroundColor: Color(0xffd3d3d3),
     appBarBackgroundColor: Colors.blue,
     primaryAccent: Color(0xfff03b8a),
+    primaryTextColor: Colors.black,
+    customGreen: Color(0xff74fe07),
+    customGrey: Color(0xff6A6A6A),
   );
 
   /// ****************************************************************
   /// Dark Theme
   /// ****************************************************************
   static const dark = CustomTheme(
-    success: Color(0xff00bc8c),
+    success: Color(0xff12af18),
     info: Color(0xff17a2b8),
     warning: Color(0xfff39c12),
     danger: Color(0xffe74c3c),
     scaffoldBackgroundColor: Color(0xff1d1d1d),
     appBarBackgroundColor: Color(0xff2F2F2F),
     primaryAccent: Color(0xfff03b8a),
+    primaryTextColor: Colors.white,
+    customGreen: Color(0xff74fe07),
+    customGrey: Color(0xff6A6A6A),
   );
 }

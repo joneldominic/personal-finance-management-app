a3837441fbb40322bbdee4337b7f280eb4a1448c
Jonel Dominic Tapang <joneldominictapang.dev@gmail.com>
2022-09-05 23:53:27 +0800

    Integrate AccountSettingsView with AccountsCardView

---
+++ lib/ui/views/accounts_card/accounts_card_view.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:personal_finance_management_app/ui/components/account_thumbnail.dart';
+import 'package:personal_finance_management_app/ui/themes/custom_theme.dart';
+import 'package:personal_finance_management_app/ui/views/accounts_card/accounts_card_viewmodel.dart';
+import 'package:stacked/stacked.dart';
+
+class AccountsCard extends StatelessWidget {
+  const AccountsCard({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final customTheme = Theme.of(context).extension<CustomTheme>()!;
+
+    return ViewModelBuilder<AccountsCardViewModel>.nonReactive(
+      viewModelBuilder: () => AccountsCardViewModel(),
+      builder: (context, model, child) => Card(
+        margin: const EdgeInsets.fromLTRB(10, 10, 10, 5),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            ListTile(
+              contentPadding: const EdgeInsets.fromLTRB(16, 0, 0, 0),
+              title: const Text(
+                'Accounts',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              trailing: IconButton(
+                icon: Icon(
+                  Icons.settings,
+                  size: 18,
+                  color: customTheme.primaryTextColor,
+                ),
+                onPressed: model.navigateToAccountSettings,
+              ),
+            ),
+            GridView.count(
+              shrinkWrap: true,
+              primary: false,
+              padding: const EdgeInsets.fromLTRB(15, 0, 15, 15),
+              crossAxisSpacing: 10,
+              mainAxisSpacing: 10,
+              crossAxisCount: 3,
+              childAspectRatio: 2.22,
+              children: const <Widget>[
+                AccountThumbnail(
+                  label: "Cash",
+                  amount: "PHP 1,500.00",
+                  color: Colors.red,
+                ),
+                AccountThumbnail(
+                  label: "BPI",
+                  amount: "PHP 2,000.00",
+                  color: Colors.amber,
+                ),
+                AccountThumbnail(
+                  label: "GCash",
+                  amount: "PHP 5,155.00",
+                  color: Colors.blue,
+                ),
+                AccountThumbnail(
+                  label: "CIMB",
+                  amount: "PHP 2,202.00",
+                  color: Colors.green,
+                ),
+                AccountThumbnail(
+                  label: "Dummy Account A",
+                  amount: "PHP 5,202.00",
+                  color: Colors.brown,
+                ),
+                AccountThumbnail(
+                  label: "Account B",
+                  amount: "PHP 5,202.00",
+                  color: Colors.deepPurple,
+                ),
+                AccountThumbnail(isAddAccount: true),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/ui/views/accounts_card/accounts_card_viewmodel.dart
@@ -0,0 +1,15 @@
+import 'package:personal_finance_management_app/app/app.locator.dart';
+import 'package:personal_finance_management_app/app/app.router.dart';
+import 'package:stacked/stacked.dart';
+import 'package:stacked_services/stacked_services.dart';
+
+// ViewModel: Manages the state of the View,
+// business logic, and any other logic as required from user interaction.
+// It does this by making use of the services
+
+class AccountsCardViewModel extends BaseViewModel {
+  final _navigationService = locator<NavigationService>();
+
+  void navigateToAccountSettings() =>
+      _navigationService.navigateTo(Routes.accountSettingsView);
+}

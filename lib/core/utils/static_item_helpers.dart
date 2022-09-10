eecf9776fac02c10070168cfda529a98a0d7af20
Jonel Dominic Tapang <joneldominictapang.dev@gmail.com>
2022-09-10 23:02:40 +0800

    Added desired color choices

--- lib/core/utils/static_item_helpers.dart
+++ lib/core/utils/static_item_helpers.dart
@@ -1,14 +1,197 @@
 import 'package:stacked/stacked_annotations.dart';
 
-// TODO: Add all necessary colors
 const List<StaticDropdownItem> colorStaticDropdownItems = [
   StaticDropdownItem(
-    title: '0xff74fe07',
-    value: '0xff74fe07',
+    title: '0xFFFF4081',
+    value: '0xFFFF4081',
   ),
   StaticDropdownItem(
-    title: '0xfff03b8a',
-    value: '0xfff03b8a',
+    title: '0xFFF50057',
+    value: '0xFFF50057',
+  ),
+  StaticDropdownItem(
+    title: '0xFFC51162',
+    value: '0xFFC51162',
+  ),
+  StaticDropdownItem(
+    title: '0xFFFF5252',
+    value: '0xFFFF5252',
+  ),
+  StaticDropdownItem(
+    title: '0xFFFF1744',
+    value: '0xFFFF1744',
+  ),
+  StaticDropdownItem(
+    title: '0xFFD50000',
+    value: '0xFFD50000',
+  ),
+  StaticDropdownItem(
+    title: '0xFFFF6E40',
+    value: '0xFFFF6E40',
+  ),
+  StaticDropdownItem(
+    title: '0xFFFF3D00',
+    value: '0xFFFF3D00',
+  ),
+  StaticDropdownItem(
+    title: '0xFFDD2C00',
+    value: '0xFFDD2C00',
+  ),
+  StaticDropdownItem(
+    title: '0xFFFFAB40',
+    value: '0xFFFFAB40',
+  ),
+  StaticDropdownItem(
+    title: '0xFFFF9100',
+    value: '0xFFFF9100',
+  ),
+  StaticDropdownItem(
+    title: '0xFFFF6D00',
+    value: '0xFFFF6D00',
+  ),
+  StaticDropdownItem(
+    title: '0xFFFFD740',
+    value: '0xFFFFD740',
+  ),
+  StaticDropdownItem(
+    title: '0xFFFFC400',
+    value: '0xFFFFC400',
+  ),
+  StaticDropdownItem(
+    title: '0xFFFFAB00',
+    value: '0xFFFFAB00',
+  ),
+  StaticDropdownItem(
+    title: '0xFFFFFF00',
+    value: '0xFFFFFF00',
+  ),
+  StaticDropdownItem(
+    title: '0xFFFFEA00',
+    value: '0xFFFFEA00',
+  ),
+  StaticDropdownItem(
+    title: '0xFFFFD600',
+    value: '0xFFFFD600',
+  ),
+  StaticDropdownItem(
+    title: '0xFFEEFF41',
+    value: '0xFFEEFF41',
+  ),
+  StaticDropdownItem(
+    title: '0xFFC6FF00',
+    value: '0xFFC6FF00',
+  ),
+  StaticDropdownItem(
+    title: '0xFFAEEA00',
+    value: '0xFFAEEA00',
+  ),
+  StaticDropdownItem(
+    title: '0xFFB2FF59',
+    value: '0xFFB2FF59',
+  ),
+  StaticDropdownItem(
+    title: '0xFF76FF03',
+    value: '0xFF76FF03',
+  ),
+  StaticDropdownItem(
+    title: '0xFF64DD17',
+    value: '0xFF64DD17',
+  ),
+  StaticDropdownItem(
+    title: '0xFF69F0AE',
+    value: '0xFF69F0AE',
+  ),
+  StaticDropdownItem(
+    title: '0xFF00E676',
+    value: '0xFF00E676',
+  ),
+  StaticDropdownItem(
+    title: '0xFF00C853',
+    value: '0xFF00C853',
+  ),
+  StaticDropdownItem(
+    title: '0xFF40C4FF',
+    value: '0xFF40C4FF',
+  ),
+  StaticDropdownItem(
+    title: '0xFF00B0FF',
+    value: '0xFF00B0FF',
+  ),
+  StaticDropdownItem(
+    title: '0xFF0091EA',
+    value: '0xFF0091EA',
+  ),
+  StaticDropdownItem(
+    title: '0xFF448AFF',
+    value: '0xFF448AFF',
+  ),
+  StaticDropdownItem(
+    title: '0xFF2979FF',
+    value: '0xFF2979FF',
+  ),
+  StaticDropdownItem(
+    title: '0xFF2962FF',
+    value: '0xFF2962FF',
+  ),
+  StaticDropdownItem(
+    title: '0xFF536DFE',
+    value: '0xFF536DFE',
+  ),
+  StaticDropdownItem(
+    title: '0xFF3D5AFE',
+    value: '0xFF3D5AFE',
+  ),
+  StaticDropdownItem(
+    title: '0xFF304FFE',
+    value: '0xFF304FFE',
+  ),
+  StaticDropdownItem(
+    title: '0xFFE040FB',
+    value: '0xFFE040FB',
+  ),
+  StaticDropdownItem(
+    title: '0xFFD500F9',
+    value: '0xFFD500F9',
+  ),
+  StaticDropdownItem(
+    title: '0xFFAA00FF',
+    value: '0xFFAA00FF',
+  ),
+  StaticDropdownItem(
+    title: '0xFF7C4DFF',
+    value: '0xFF7C4DFF',
+  ),
+  StaticDropdownItem(
+    title: '0xFF651FFF',
+    value: '0xFF651FFF',
+  ),
+  StaticDropdownItem(
+    title: '0xFF6200EA',
+    value: '0xFF6200EA',
+  ),
+  StaticDropdownItem(
+    title: '0xFF8D6E63',
+    value: '0xFF8D6E63',
+  ),
+  StaticDropdownItem(
+    title: '0xFF795548',
+    value: '0xFF795548',
+  ),
+  StaticDropdownItem(
+    title: '0xFF6D4C41',
+    value: '0xFF6D4C41',
+  ),
+  StaticDropdownItem(
+    title: '0xFFBDBDBD',
+    value: '0xFFBDBDBD',
+  ),
+  StaticDropdownItem(
+    title: '0xFF757575',
+    value: '0xFF757575',
+  ),
+  StaticDropdownItem(
+    title: '0xFF616161',
+    value: '0xFF616161',
   ),
 ];
 

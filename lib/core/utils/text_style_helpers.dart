db9704c1345e317d34e773b305579ddd2fa594e7
Jonel Dominic Tapang <joneldominictapang.dev@gmail.com>
2022-09-18 12:22:24 +0800

    Added Text Style Helpers

---
+++ lib/core/utils/text_style_helpers.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/cupertino.dart';
+
+// TODO: Apply this styling on necessary TextStyles
+// Text Styles
+
+// To make it clear which weight we are using, we'll define the weight even for regular
+// fonts
+const TextStyle heading1Style = TextStyle(
+  fontSize: 34,
+  fontWeight: FontWeight.w400,
+);
+
+const TextStyle heading2Style = TextStyle(
+  fontSize: 28,
+  fontWeight: FontWeight.w600,
+);
+
+const TextStyle heading3Style = TextStyle(
+  fontSize: 24,
+  fontWeight: FontWeight.w600,
+);
+
+const TextStyle heading4Style = TextStyle(
+  fontSize: 14,
+  fontWeight: FontWeight.w600,
+);
+
+const TextStyle headlineStyle = TextStyle(
+  fontSize: 30,
+  fontWeight: FontWeight.w700,
+);
+
+const TextStyle bodyStyle = TextStyle(
+  fontSize: 16,
+  fontWeight: FontWeight.w400,
+);
+
+const TextStyle subheadingStyle = TextStyle(
+  fontSize: 20,
+  fontWeight: FontWeight.w400,
+);
+
+const TextStyle captionStyle = TextStyle(
+  fontSize: 12,
+  fontWeight: FontWeight.w400,
+);

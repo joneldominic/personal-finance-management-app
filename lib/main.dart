199c9a1380212658a0d769d329f99869fd3cdf83
Jonel Dominic Tapang <joneldominictapang.dev@gmail.com>
2022-12-17 13:04:16 +0800

    Additional dialogSetup

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:logger/logger.dart';
 import 'package:personal_finance_management_app/app/app.locator.dart';
+import 'package:personal_finance_management_app/ui/setup/setup_dialog_ui.dart';
 
 import 'app.dart';
 
-void main() {
+Future<void> main() async {
   Logger.level = Level.verbose;
-  setupLocator();
+  await setupLocator();
+  setupDialogUi();
   runApp(const PersonalFinanceManagementApp());
 }

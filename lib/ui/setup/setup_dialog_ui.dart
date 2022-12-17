d5ec4a64ef0fc3f0543b731ffb0888abc4a39bf8
Jonel Dominic Tapang <joneldominictapang.dev@gmail.com>
2022-12-17 13:40:07 +0800

    Added DeleteConfirmationDialog

--- lib/ui/setup/setup_dialog_ui.dart
+++ lib/ui/setup/setup_dialog_ui.dart
@@ -1,6 +1,7 @@
 import 'package:personal_finance_management_app/app/app.locator.dart';
 import 'package:personal_finance_management_app/core/enums/dialog_type.dart';
 import 'package:personal_finance_management_app/ui/dialogs/balance_confirmation_dialog.dart';
+import 'package:personal_finance_management_app/ui/dialogs/delete_confirmation_dialog.dart';
 import 'package:stacked_services/stacked_services.dart';
 
 void setupDialogUi() {
@@ -9,6 +10,8 @@ void setupDialogUi() {
   final Map<dynamic, DialogBuilder> builders = {
     DialogType.balanceUpdateConfirmation: (context, sheetRequest, completer) =>
         BalanceConfirmationDialog(request: sheetRequest, completer: completer),
+    DialogType.deleteConfirmation: (context, sheetRequest, completer) =>
+        DeleteConfirmationDialog(request: sheetRequest, completer: completer),
   };
 
   dialogService.registerCustomDialogBuilders(builders);

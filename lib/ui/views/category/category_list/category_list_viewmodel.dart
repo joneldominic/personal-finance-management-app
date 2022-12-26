e04458c147cc0bf78778014e27eef1256cfbfebb
Jonel Dominic Tapang <joneldominictapang.dev@gmail.com>
2022-12-26 21:17:20 +0800

    Make category list view dynamic

--- lib/ui/views/category/category_list/category_list_viewmodel.dart
+++ lib/ui/views/category/category_list/category_list_viewmodel.dart
@@ -1,6 +1,9 @@
 import 'package:personal_finance_management_app/app/app.locator.dart';
 import 'package:personal_finance_management_app/app/app.logger.dart';
 import 'package:personal_finance_management_app/app/app.router.dart';
+import 'package:personal_finance_management_app/data/models/category/category.dart';
+import 'package:personal_finance_management_app/extensions/list_extension.dart';
+import 'package:personal_finance_management_app/services/category_service.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
 
@@ -8,14 +11,28 @@ import 'package:stacked_services/stacked_services.dart';
 // business logic, and any other logic as required from user interaction.
 // It does this by making use of the services
 
-class CategoryListViewModel extends BaseViewModel {
+class CategoryListViewModel extends StreamViewModel<List<Category>> {
   final _logger = getLogger('CategoryListViewModel');
   final _navigationService = locator<NavigationService>();
+  final _categoryService = locator<CategoryService>();
 
-  void navigateToCategoryDetail({bool isAddCategory = true}) {
-    _logger.i('argument: {isAddCategory: $isAddCategory}');
+  List<Category> categories = [];
+
+  @override
+  Stream<List<Category>> get stream =>
+      _categoryService.categoryCollectionStream();
+
+  @override
+  void onData(List<Category>? data) {
+    _logger.i('argument: ${data?.itemsToString()}');
+    categories = data ?? [];
+    notifyListeners();
+  }
+
+  void navigateToCategoryDetail(Category? category) {
+    _logger.i('argument: {category: $category}');
     _navigationService.navigateToCategoryDetailView(
-      isAddCategory: isAddCategory,
+      category: category,
     );
   }
 }

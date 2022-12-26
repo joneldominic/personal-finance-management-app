3b0f024621efb951a09e4618d3c938aa01f4c4ed
Jonel Dominic Tapang <joneldominictapang.dev@gmail.com>
2022-12-26 21:36:24 +0800

    Added logging on transaction dao implementation

--- lib/data/dao/transaction_dao_impl.dart
+++ lib/data/dao/transaction_dao_impl.dart
@@ -1,16 +1,20 @@
 import 'package:isar/isar.dart';
+import 'package:personal_finance_management_app/app/app.logger.dart';
 import 'package:personal_finance_management_app/data/dao/transaction_dao.dart';
 import 'package:personal_finance_management_app/data/database/isar_database.dart';
 import 'package:personal_finance_management_app/data/models/transaction/transaction.dart';
 
 class TransactionDaoImpl extends TransactionDao {
+  final _logger = getLogger('TransactionDaoImpl');
+
   Future<Isar> get _db async => await IsarDatabase.instance.database;
-  // TODO: Add logger
 
   @override
   Future<Transaction> createTransaction(Transaction transaction) async {
-    Isar isar = await _db;
+    _logger.i('argument: $transaction');
 
+    Isar isar = await _db;
+    
     final transactionCollection = isar.transactions;
     final createdTransaction = await isar.writeTxn(() async {
       final id = await transactionCollection.put(transaction);
@@ -22,6 +26,8 @@ class TransactionDaoImpl extends TransactionDao {
 
   @override
   Future<Transaction> getTransactionById(Id id) async {
+    _logger.i('argument: $id');
+
     Isar isar = await _db;
 
     return Future.error("");
@@ -29,6 +35,8 @@ class TransactionDaoImpl extends TransactionDao {
 
   @override
   Future<List<Transaction>> getTransactions() async {
+    _logger.i('argument: NONE');
+
     Isar isar = await _db;
 
     return Future.error("");
@@ -36,6 +44,8 @@ class TransactionDaoImpl extends TransactionDao {
 
   @override
   Future<Transaction> updateTransaction(Transaction transaction) async {
+    _logger.i('argument: $transaction');
+
     Isar isar = await _db;
 
     return Future.error("");
@@ -43,6 +53,8 @@ class TransactionDaoImpl extends TransactionDao {
 
   @override
   Future<Id> deleteTransaction(Id id) async {
+    _logger.i('argument: $id');
+
     Isar isar = await _db;
 
     return Future.error("");

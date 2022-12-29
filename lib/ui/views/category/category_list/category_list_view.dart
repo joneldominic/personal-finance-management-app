a6c035d091c10cb3b38884c345489f5aa376f41c
Jonel Dominic Tapang <joneldominictapang.dev@gmail.com>
2022-12-29 21:49:48 +0800

    CategoryListView code clean-up

--- lib/ui/views/category/category_list/category_list_view.dart
+++ lib/ui/views/category/category_list/category_list_view.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:personal_finance_management_app/core/enums/category_nature.dart';
 import 'package:personal_finance_management_app/ui/components/category_list_item.dart';
 import 'package:personal_finance_management_app/ui/components/custom_app_bar.dart';
 import 'package:personal_finance_management_app/ui/components/custom_floating_action_button.dart';
@@ -20,44 +19,42 @@ class CategoryListView extends StatelessWidget {
   Widget build(BuildContext context) {
     final customTheme = Theme.of(context).extension<CustomTheme>()!;
 
-    // TODO: Make data dynamic
-
     return ViewModelBuilder<CategoryListViewModel>.reactive(
-        viewModelBuilder: () => CategoryListViewModel(),
-        builder: (context, model, child) => Scaffold(
-              floatingActionButton: CustomFloatingActionButton(
-                icon: const Icon(Icons.add_rounded),
-                label: "Add Category",
-                onPressed: () => model.navigateToCategoryDetail(null),
-              ),
-              appBar: const CustomAppBar(
-                title: Text("Categories"),
-              ),
-              body: Container(
-                color: customTheme.contrastBackgroundColor,
-                child: !model.isBusy
-                    ? ListView.separated(
-                        padding: const EdgeInsets.fromLTRB(0, 10, 0, 90),
-                        physics: const BouncingScrollPhysics(),
-                        itemCount: model.categories.length,
-                        itemBuilder: (BuildContext context, int index) {
-                          return CategoryListItem(
-                            categoryName: model.categories[index].name!,
-                            categoryNature:
-                                model.categories[index].nature!,
-                            color: Color(
-                              int.parse(model.categories[index].color!),
-                            ),
-                            onPressed: () => model.navigateToCategoryDetail(
-                              model.categories[index],
-                            ),
-                          );
-                        },
-                        separatorBuilder: (BuildContext context, int index) =>
-                            const Divider(),
-                      )
-                    : const Center(child: CircularProgressIndicator()),
-              ),
-            ));
+      viewModelBuilder: () => CategoryListViewModel(),
+      builder: (context, model, child) => Scaffold(
+        floatingActionButton: CustomFloatingActionButton(
+          icon: const Icon(Icons.add_rounded),
+          label: "Add Category",
+          onPressed: () => model.navigateToCategoryDetail(null),
+        ),
+        appBar: const CustomAppBar(
+          title: Text("Categories"),
+        ),
+        body: Container(
+          color: customTheme.contrastBackgroundColor,
+          child: !model.isBusy
+              ? ListView.separated(
+                  padding: const EdgeInsets.fromLTRB(0, 10, 0, 90),
+                  physics: const BouncingScrollPhysics(),
+                  itemCount: model.categories.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return CategoryListItem(
+                      categoryName: model.categories[index].name!,
+                      categoryNature: model.categories[index].nature!,
+                      color: Color(
+                        int.parse(model.categories[index].color!),
+                      ),
+                      onPressed: () => model.navigateToCategoryDetail(
+                        model.categories[index],
+                      ),
+                    );
+                  },
+                  separatorBuilder: (BuildContext context, int index) =>
+                      const Divider(),
+                )
+              : const Center(child: CircularProgressIndicator()),
+        ),
+      ),
+    );
   }
 }

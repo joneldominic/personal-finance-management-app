9b032331c44a00dae64d6e7302a0646cbdd5270a
Jonel Dominic Tapang <joneldominictapang.dev@gmail.com>
2023-01-01 02:07:44 +0800

    Added ConditionalAsyncWrapper

---
+++ lib/ui/components/conditional_async_wrapper.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ConditionalAsyncWrapper extends StatelessWidget {
+  const ConditionalAsyncWrapper({
+    Key? key,
+    required this.isLoading,
+    required this.child,
+  }) : super(key: key);
+
+  final bool isLoading;
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return isLoading ? const CircularProgressIndicator() : child;
+  }
+}

be5bd56220555602967c0bc802ecea1f47e466fe
Jonel Dominic Tapang <joneldominictapang.dev@gmail.com>
2023-01-30 12:32:10 +0800

    Added FROM_ACCOUNT_CHANGE_NOTE constant

--- lib/core/utils/app_constants.dart
+++ lib/core/utils/app_constants.dart
@@ -1,4 +1,6 @@
 // ignore_for_file: constant_identifier_names
 
+/// Transactions Related Constants
 const TRANSFER_CATEGORY_NAME = "Transfer";
 const UNDEFINED_CATEGORY_NAME = "Undefined";
+const FROM_ACCOUNT_CHANGE_NOTE = "From Account Change";

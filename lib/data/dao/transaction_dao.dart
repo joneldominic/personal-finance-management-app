728c6e5eb2107c8b0cb7ea17cf125f45778561a5
Jonel Dominic Tapang <joneldominictapang.dev@gmail.com>
2023-02-28 11:45:17 +0800

    Added recent transaction related dao

--- lib/data/dao/transaction_dao.dart
+++ lib/data/dao/transaction_dao.dart
@@ -6,10 +6,12 @@ abstract class TransactionDao {
   Future<List<Transaction>> createTransactions(List<Transaction> transactions);
   Future<Transaction> getTransactionById(Id id);
   Future<List<Transaction>> getTransactions();
+  Future<List<Transaction>> getRecentTransactions();
   Future<List<Transaction>> getTransactionsByTransferId(String transferId);
   Future<Transaction> updateTransaction(Transaction transaction);
   Future<Id> deleteTransaction(Id id);
   Future<int> deleteTransactionsByTransferId(String transferId);
   Future<int> deleteTransactions(List<int> ids);
   Stream<List<Transaction>> transactionCollectionStream();
+  Stream<List<Transaction>> recentTransactionCollectionStream();
 }

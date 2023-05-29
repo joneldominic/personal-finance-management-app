119b5d72b5eff00c0a51ada6f22715f0246fe822
Jonel Dominic Tapang <joneldominictapang.dev@gmail.com>
2023-05-29 14:04:01 +0800

    Added about app content in about view

--- lib/ui/views/about/about_view.dart
+++ lib/ui/views/about/about_view.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:personal_finance_management_app/core/utils/app_constants.dart';
 import 'package:personal_finance_management_app/core/utils/text_style_helpers.dart';
@@ -29,7 +30,6 @@ class AboutView extends StatelessWidget {
           appBar: const CustomAppBar(
             title: Text("About"),
           ),
-          backgroundColor: customTheme.contrastBackgroundColor,
           body: SingleChildScrollView(
             physics: const BouncingScrollPhysics(),
             child: Column(
@@ -52,6 +52,8 @@ class AboutView extends StatelessWidget {
                   "Version $appVersion",
                   style: cardSubTitleStyle,
                 ),
+                verticalSpaceRegular,
+                _buildAboutContent(model: model, customTheme: customTheme),
               ],
             ),
           ),
@@ -59,4 +61,83 @@ class AboutView extends StatelessWidget {
       },
     );
   }
+
+  TextSpan _buildTextSpanLink({
+    required String text,
+    required Uri uri,
+    required void Function() onTap,
+  }) {
+    return TextSpan(
+      text: text,
+      style: const TextStyle(color: Colors.blue),
+      recognizer: TapGestureRecognizer()..onTap = onTap,
+    );
+  }
+
+  Widget _buildAboutContent({
+    required AboutViewModel model,
+    required CustomTheme customTheme,
+  }) {
+    final walletAppURI = Uri.parse("https://budgetbakers.com/");
+    final splitwiseAppURI = Uri.parse("https://www.splitwise.com/");
+    final bucketsAppURI = Uri.parse("https://www.budgetwithbuckets.com/");
+
+    final style = TextStyle(color: customTheme.primaryTextColor);
+
+    return _buildCard(
+      children: [
+        RichText(
+          textAlign: TextAlign.justify,
+          text: TextSpan(
+            children: [
+              TextSpan(
+                text: "An open-source, free budgeting app inspired by a popular apps, like ",
+                style: style,
+              ),
+              _buildTextSpanLink(
+                text: "Wallet",
+                uri: walletAppURI,
+                onTap: () => model.onTapLink(walletAppURI),
+              ),
+              TextSpan(
+                text: ", ",
+                style: style,
+              ),
+              _buildTextSpanLink(
+                text: "Splitwise",
+                uri: splitwiseAppURI,
+                onTap: () => model.onTapLink(splitwiseAppURI),
+              ),
+              TextSpan(
+                text: " and ",
+                style: style,
+              ),
+              _buildTextSpanLink(
+                text: "Buckets",
+                uri: bucketsAppURI,
+                onTap: () => model.onTapLink(bucketsAppURI),
+              ),
+              TextSpan(
+                text:
+                    ". Designed to provide an easy-to-use solution for managing personal finances. The app allows users to categorize expenses, set budgets, and track spending, as well as reach savings goals. With a user-friendly interface, this app is perfect for individuals looking to take control of their finances.",
+                style: style,
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+Widget _buildCard({required List<Widget> children}) {
+  return Card(
+    margin: const EdgeInsets.all(10),
+    child: Padding(
+      padding: const EdgeInsets.all(10),
+      child: Column(
+        children: children,
+      ),
+    ),
+  );
 }
--- lib/ui/views/about/about_viewmodel.dart
+++ lib/ui/views/about/about_viewmodel.dart
@@ -1,6 +1,10 @@
 import 'package:package_info_plus/package_info_plus.dart';
+import 'package:personal_finance_management_app/app/app.locator.dart';
 import 'package:personal_finance_management_app/app/app.logger.dart';
+import 'package:personal_finance_management_app/core/enums/snackbar_type.dart';
 import 'package:stacked/stacked.dart';
+import 'package:stacked_services/stacked_services.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 // ViewModel: Manages the state of the View,
 // business logic, and any other logic as required from user interaction.
@@ -8,6 +12,7 @@ import 'package:stacked/stacked.dart';
 
 class AboutViewModel extends BaseViewModel {
   final _logger = getLogger('AboutViewModel');
+  final _snackbarService = locator<SnackbarService>();
 
   PackageInfo? packageInfo;
 
@@ -18,5 +23,22 @@ class AboutViewModel extends BaseViewModel {
     _logger.d(packageInfo);
 
     notifyListeners();
+  }
+
+  void onTapLink(Uri uri) async {
+    _logger.i('argument: $uri');
+
+    try {
+      await launchUrl(uri);
+      _logger.i('Link Opened: $uri');
+    } on Exception catch (_, e) {
+      _logger.e("Failed to open link with error: $e");
+      _snackbarService.showCustomSnackBar(
+        variant: SnackbarType.main,
+        message: "Failed to open link. Please try again.",
+        duration: const Duration(seconds: 2),
+        onTap: () {},
+      );
+    }
   }
 }

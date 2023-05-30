ad5387b4aefb3db63cdccdd71732eb91df87cd77
Jonel Dominic Tapang <joneldominictapang.dev@gmail.com>
2023-05-31 02:38:38 +0800

    Added TIME_PERIOD_MENU_ITEMS popup menu constants

--- lib/core/utils/app_constants.dart
+++ lib/core/utils/app_constants.dart
@@ -1,6 +1,8 @@
 // ignore_for_file: constant_identifier_names
 
 import 'package:flutter/widgets.dart';
+import 'package:personal_finance_management_app/core/enums/time_period.dart';
+import 'package:personal_finance_management_app/core/types/generic_pop_up_menu_item.dart';
 
 /// Transactions Related Constants
 const TRANSFER_CATEGORY_NAME = "Transfer";
@@ -35,3 +37,17 @@ const TRANSFER_ICON = IconData(
 /// Asset Constants
 const GOOGLE_LOGO_ASSET_IMAGE = AssetImage("assets/images/google.png");
 const APP_LOGO_ASSET_IMAGE = AssetImage("assets/images/logo.png");
+
+/// Popup Menu Constants
+const TIME_PERIOD_MENU_ITEMS_SHORT = [
+  GenericPopupMenuItem(value: TimePeriod.today, displayText: 'Today'),
+  GenericPopupMenuItem(value: TimePeriod.sevenDays, displayText: '7 Days'),
+  GenericPopupMenuItem(value: TimePeriod.thirtyDays, displayText: '30 Days'),
+];
+
+const TIME_PERIOD_MENU_ITEMS_LONG = [
+  ...TIME_PERIOD_MENU_ITEMS_SHORT,
+  GenericPopupMenuItem(value: TimePeriod.sixMonths, displayText: '6 Months'),
+  GenericPopupMenuItem(value: TimePeriod.oneYear, displayText: '1 Year'),
+  GenericPopupMenuItem(value: TimePeriod.fiveYears, displayText: '5 Years'),
+];

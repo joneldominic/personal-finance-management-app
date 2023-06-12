3f45d362254bcfcb3db35aff7ac18bb862c195e2
Jonel Dominic Tapang <joneldominictapang.dev@gmail.com>
2023-06-12 22:45:18 +0800

    Refactored TimePeriod Constants; Addeed helper class

--- lib/core/enums/time_period.dart
+++ lib/core/enums/time_period.dart
@@ -1,1 +1,22 @@
 enum TimePeriod { today, sevenDays, thirtyDays, sixMonths, oneYear, fiveYears }
+
+final daysCountArr = [1, 7, 30, 180, 365, 1825];
+final titleArr = ['Today', '7 Days', '30 Days', '6 Months', '1 Year', '5 Years'];
+
+extension TimePeriodExtension on TimePeriod {
+  int get days {
+    return daysCountArr[index];
+  }
+
+  String get str {
+    return titleArr[index];
+  }
+}
+
+class TimePeriodHelper {
+  static String getTitleByDaysCount(int daysCount) {
+    Map<int, String> mergedMap =
+        Map.fromIterables(daysCountArr, List.generate(titleArr.length, (index) => titleArr[index]));
+    return mergedMap[daysCount]!;
+  }
+}
--- lib/core/utils/app_constants.dart
+++ lib/core/utils/app_constants.dart
@@ -39,15 +39,15 @@ const GOOGLE_LOGO_ASSET_IMAGE = AssetImage("assets/images/google.png");
 const APP_LOGO_ASSET_IMAGE = AssetImage("assets/images/logo.png");
 
 /// Popup Menu Constants
-const TIME_PERIOD_MENU_ITEMS_SHORT = [
-  GenericPopupMenuItem(value: TimePeriod.today, displayText: 'Today'),
-  GenericPopupMenuItem(value: TimePeriod.sevenDays, displayText: '7 Days'),
-  GenericPopupMenuItem(value: TimePeriod.thirtyDays, displayText: '30 Days'),
+final TIME_PERIOD_MENU_ITEMS_SHORT = [
+  GenericPopupMenuItem(value: TimePeriod.today, displayText: TimePeriod.today.str),
+  GenericPopupMenuItem(value: TimePeriod.sevenDays, displayText: TimePeriod.sevenDays.str),
+  GenericPopupMenuItem(value: TimePeriod.thirtyDays, displayText: TimePeriod.thirtyDays.str),
 ];
 
-const TIME_PERIOD_MENU_ITEMS_LONG = [
+final TIME_PERIOD_MENU_ITEMS_LONG = [
   ...TIME_PERIOD_MENU_ITEMS_SHORT,
-  GenericPopupMenuItem(value: TimePeriod.sixMonths, displayText: '6 Months'),
-  GenericPopupMenuItem(value: TimePeriod.oneYear, displayText: '1 Year'),
-  GenericPopupMenuItem(value: TimePeriod.fiveYears, displayText: '5 Years'),
+  GenericPopupMenuItem(value: TimePeriod.sixMonths, displayText: TimePeriod.sixMonths.str),
+  GenericPopupMenuItem(value: TimePeriod.oneYear, displayText: TimePeriod.oneYear.str),
+  GenericPopupMenuItem(value: TimePeriod.fiveYears, displayText: TimePeriod.fiveYears.str),
 ];
